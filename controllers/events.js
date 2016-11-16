cde39c105df8730915fdbd2a6b7d17bb9a12c3a7
Harshal Chopde <harshal.chopde@yale.edu>
2016-11-16 00:20:06 +0000

    Make RSVPs work

--- controllers/events.js
+++ controllers/events.js
@@ -122,11 +122,14 @@ function rsvp (request, response){
     response.status(404).send('No such event');
   }
 
-  if(validator.isEmail(request.body.email)){
+  if(validator.isEmail(request.body.email) && request.body.email.toLowerCase().indexOf("yale.edu") !== -1 ){
     ev.attending.push(request.body.email);
     response.redirect('/events/' + ev.id);
   }else{
     var contextData = {errors: [], event: ev};
+    if(request.body.email.toLowerCase().indexOf('harvard') !== -1){
+    contextData.errors.push('Invalid email, punk');  
+    }else
     contextData.errors.push('Invalid email');
     response.render('event-detail.html', contextData);    
   }
